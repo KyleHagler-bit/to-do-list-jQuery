5e9b17846beb18368a688eb1096dba52dd1ba78f
Kyle Hagler <khagler.kh@gmail.com>
2020-06-07 01:30:16 -0500

    finally got swal alert to work with past due date vs future

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,21 +8,43 @@ function onReady(){
     $(document).on("click", "#deleteButton", deleteTask);
     $(document).on("click", "#updateButton", update);
     getHistory();
+  
 } //end onReady
 
 //-------POST---------------------
-function addTask(){ 
+function addTask(event){ 
     let name = $('#taskNameIn').val();
     let notes = $('#notesIn').val();
+    let due = $('#dueDate').val();
+    
+    let now = new Date();
+    let dueMilli = new Date(due);
+
+    let setDate = dueMilli.getTime();
+    let nowDate = now.getTime();
 
     let taskObj ={
         name: name,
         notes: notes,
         date: 'Not Completed', //default for new task
+        due: due,
     };
-    if (name !=='' && name !==undefined ){
+    //getTime turns them to millseconds and then compares numbers
+    if (setDate < nowDate){ //do not let user enter past date for due date
+      event.preventDefault(); //stops page from "passing by" swalAlert
+      swal({
+        position: 'top-end',
+        icon: 'error',
+        title: 'Past dates cannot be entered',
+        // showConfirmButton: false,
+        timer: 3000,
+      })
+      
+    }
+      else if (name !=='' && name !==undefined &&(setDate >= nowDate)){
         $('#taskNameIn').val('');//clears input
         $('#notesIn').val('');
+        $('#dueDate').val('');
         
         $.ajax({
             method: 'POST',
@@ -56,12 +78,16 @@ function getHistory(){
         
         for (let i = 0; i < response.length; i++) {
             let task = response[i];
-            console.log(task.date);
+          
+            let formattedDue = new Date(task.due)
+            formattedDue = formatDate(formattedDue); //make it into a more friendly format
+           
 
             let rowElement = $('<tr></tr>');
             if (task.date === 'Not Completed'){
                 rowElement.append(`<td scope='row'>${task.name}</td>`);
                 rowElement.append(`<td>${task.notes}</td>`);
+                rowElement.append(`<td style='text-align:center'>${formattedDue}</td>`)
                 rowElement.append(`<td style='text-align:center' id = 'status'>${task.date}</td>`);
                 rowElement.append(`<td><button class='btn btn-success btn-sm' id="updateButton" data-id='${task.id}'>Mark Complete</button></td>`);
                 rowElement.append(`<td class ='lastColumn'><button class="btn btn-danger btn-sm" id='deleteButton' data-id='${task.id}'>Delete Task</button></td>`);
@@ -69,6 +95,7 @@ function getHistory(){
                 
                 rowElement.append(`<td>${task.name}</td>`);
                 rowElement.append(`<td>${task.notes}</td>`);
+                rowElement.append(`<td></td>`)
                 rowElement.append(`<td style='text-align:center' id = 'status'>${task.date}</td>`);
                 rowElement.append(`<td><img src='images/checkmark_trim.png' id="checkmark"/></td>`);
                 rowElement.append(`<td class ='lastColumn'><button class="btn btn-danger btn-sm" id="deleteButton" data-id='${task.id}'>Delete Task</button></td>`);
@@ -104,21 +131,22 @@ function deleteTask(){
           icon: "success",
         });
       } else {
-        swal("Task remains");
+        swal("Task not deleted");
         return;
       }
     });
 }//end deleteTask
 
+//----PUT-------
 function update() {
     const element = event.target;
     let taskId = $(element).data().id;
-    let date = new Date();
+    let date = new Date(); //get current date
     console.log(date);
     date = date.toString(); //make sure it is a string
    
-    date = date.slice(0,15); //cuts off time stamp
-    console.log(date);
+    date = date.slice(0,15); //cuts off time stamp (DAY MON Do YYYY)
+    
     
 
     let updateObj={
@@ -136,3 +164,37 @@ function update() {
     });
   }
 
+  //-------TIME STUFF BELOW--------------------------------
+function formatDate(due){
+  let weekDay = getWeekDay(due);
+  let month = getMonth(due);
+  let year = due.getFullYear();
+  let day=due.getDate();
+  
+  if (day <=9){
+    let strDate = (`${weekDay} ${month} 0${day} ${year}`); //make sure it matches other format
+  return strDate;
+  }else{
+  let strDate = (`${weekDay} ${month} ${day} ${year}`);
+  return strDate;
+  }
+}
+
+function getWeekDay(due){
+  //Create an array containing each day, starting with Sunday.
+  let weekdays = [
+      "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+  ];
+  //Use the getDay() method to get the day.
+  let day = due.getDay();
+  day=weekdays[day];
+  //Return the element that corresponds to that index.
+  return day;
+}
+
+function getMonth(due){
+  let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  let month = due.getMonth();
+  month = months[month];
+  return month;
+}
